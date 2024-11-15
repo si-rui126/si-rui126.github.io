90304799e04f41f2444a4f2876ffab3c341a569d
si-rui126 <65576101+si-rui126@users.noreply.github.com>
2024-11-14 17:53:42 -0800

    added arch portfolio

--- components/scripts.js
+++ components/scripts.js
@@ -13,3 +13,21 @@ function show_descr(img_id, word_id) {
         word.style.opacity = 0;
     }
 }
+
+// show more / show less function
+function show_moreless(dots_id, text_id, btn_id) {
+    var dots = document.getElementById(dots_id);
+    var text = document.getElementById(text_id);
+    var btn = document.getElementById(btn_id);
+
+    if (dots.style.display === "none") {
+        dots.style = "inline";
+        btn.innerHTML = "Read more";
+        text.style.display = "none";
+    }
+    else {
+        dots.style.display = "none";
+        btn.innerHTML = "Read less";
+        text.style.display = "inline";
+      }
+}
