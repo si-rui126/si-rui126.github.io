760024e5e05c256f79b6efbe0341c5797149b4b3
si-rui126 <65576101+si-rui126@users.noreply.github.com>
2025-01-14 23:18:50 -0800

    BIG theme overhaul + new features

--- components/header.js
+++ components/header.js
@@ -5,28 +5,48 @@ class Header extends HTMLElement {
 
 connectedCallback() {
     this.innerHTML = `
-        <style>
-        hr {
-            line-width: 2px;
+    <style>
+        html {
+            --font-fam: Verdana,sans-serif;
         }
 
-        .mybar {
-            overflow: hidden;
-            font-family: monospace, sans-serif;
+        /* Theme toggle */
+        body {
+            background-color: rgb(27, 26, 26);
+            color: white;
+        }
+
+        .light-mode {
+            background-color: white;
+            color : rgb(41, 41, 41);
         }
 
         /* Navbar container */
         .navbar {
             overflow: hidden;
-            font-family: monospace, sans-serif;
-            padding: 2%;
+            font-family: var(font-fam);
+            padding: 1%;
         }       
 
     /* Links inside the navbar */
+        .navbar navtext {
+            font-family: var(font-fam);
+            float: center;
+            font-size: 24px;
+            color:rgb(255, 0, 0);
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        .navbar .mode-toggle {
+            background-color: none;
+        }
+
         .navbar a {
-            float: left;
+            float: center;
             font-size: 20px;
-            color: #456061;
+            color: #aaabac;
             text-align: center;
             padding: 14px 16px;
             text-decoration: none;
@@ -48,15 +68,17 @@ connectedCallback() {
         .span{
             font-family: 'Consolas';
         } 
-
     </style>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="./components/scripts.js" type="text/javascript" defer></script>
     
     <div class="navbar" id="MyTopnav">
-        <a href="./index.html" class="active">About</a>
-        <a href="./projects.html" class="active">Projects</a>
-        <a href="./resume.html" class="active">Resume</a>     
+        <navtext>Marian Lu Portfolio</navtext>
+        <p><a style="float: center" href="./index.html" class="active">About</a>
+        <a style="float: center" href="./projects.html" class="active">Projects</a>
+        <a style="float: center" href="./resume.html" class="active">Resume</a>
+        <button style="float: right;" class="mode-toggle"><i style="color:rgb(255, 0, 0);" class="fa-solid fa-sun fa-xl"></i></button>     
     </div>
     </header>
     `;
--- components/scripts.js
+++ components/scripts.js
@@ -1,9 +1,81 @@
+/**************** Theme script ****************/
+const buttons = document.querySelectorAll('.mode-toggle')
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function (){
+        document.body.classList.toggle('light-mode')
+        document.querySelectorAll('.fa-sun').forEach(icon => icon.classList.toggle('fa-moon'))
+
+        if (document.body.classList.contains('light-mode')) {
+            localStorage.setItem('lightMode', 'enabled')
+          } else {
+            localStorage.setItem('lightMode', 'disabled')
+          }
+    })
+})
+
+if (localStorage.getItem('lightMode') === 'enabled') {
+    document.body.classList.add('light-mode')
+    document.querySelectorAll('.fa-sun').forEach(icon => icon.classList.add('fa-moon'))
+  }
+
+/*************** filters projects *****************/
+filter_proj("all")
+function filter_proj(c) {
+  var x, i;
+  x = document.getElementsByClassName("project-cont");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("btnContainer");
+var btns = btnContainer.getElementsByClassName("filter-btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    // var current = document.getElementsByClassName("active");
+    // current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+
 // Displays project decription when image is clicked (for mobile users)
 function show_descr(img_id, word_id) {
     img = document.getElementById(img_id);
     word = document.getElementById(word_id);
     if (word.style.visibility === "hidden") {
-        img.style.filter = "brightness(0.4)";
+        img.style.filter = "brightness(0.3)";
         word.style.visibility = "visible";
         word.style.opacity = 1;
     }
@@ -14,7 +86,7 @@ function show_descr(img_id, word_id) {
     }
 }
 
-// show more / show less function
+/*************** show more / show less function***************/
 function show_moreless(dots_id, text_id, btn_id) {
     var dots = document.getElementById(dots_id);
     var text = document.getElementById(text_id);
@@ -31,3 +103,4 @@ function show_moreless(dots_id, text_id, btn_id) {
         text.style.display = "inline";
       }
 }
+
